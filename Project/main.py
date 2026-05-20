737cdb0057139ad4d38024e3631068e2bc16e5fa
bkphn <bkphn011@gmail.com>
2026-05-20 16:34:34 +0200

    Added preprocessing and saving data to file

--- Project/main.py
+++ Project/main.py
@@ -1,6 +1,42 @@
+import os
+import numpy as np
+
+from preprocessing import preprocess_data
+
+# ----- MODEL SETTINGS -----
+PREPROCESSING = True
+
+# ----- CMD COLOR SETTINGS -----
 RED = '\033[91m \x1B[3m'
 YELLOW = '\033[93m \x1B[3m'
 GREEN = '\033[92m \x1B[3m'
 RESET = '\033[0m \x1B[0m'
 
-print("Hello, World!")
+# ----- PATHS TO DATA -----
+TRAIN_PATH = os.path.join("Data", "train")
+TEST_PATH = os.path.join("Data", "test")
+
+# ------------------------------------------------------------------------------------------------
+if __name__ == '__main__':
+    emotions_map = {"angry": 0, "disgust": 1, "fear": 2, "happy": 3,
+                    "neutral": 4, "sad": 5, "surprise": 6}
+
+    if PREPROCESSING:
+        print(f"{GREEN}Rozpoczynam ekstrakcję cech HOG...\n{RESET}")
+        train_features, train_labels = preprocess_data(emotions_map, TRAIN_PATH)
+        test_features, test_labels = preprocess_data(emotions_map, TEST_PATH)
+        print(f"{GREEN}Ekstrakcja cech zakończona pomyślnie.{RESET}")
+
+        print(f"{GREEN}\nGeneruję macierze cech HOG i wektory etykiet...{RESET}")
+        X_train = np.array(train_features)
+        y_train = np.array(train_features)
+        X_test = np.array(test_features)
+        y_test = np.array(test_features)
+        print(f"{GREEN}Pomyślnie stworzono i zapisano pliki.{RESET}")
+
+        print(f"{GREEN}\nZapisuję dane do pliku...{RESET}")
+        np.save(os.path.join("Model", "X_train.npy"), X_train)
+        np.save(os.path.join("Model", "y_train.npy"), y_train)
+        np.save(os.path.join("Model", "X_test.npy"), X_test)
+        np.save(os.path.join("Model", "y_test.npy"), y_test)
+        print(f"{GREEN}Dane zapisane pomyślnie.{RESET}")
---
+++ Project/preprocessing.py
@@ -0,0 +1,39 @@
+import os
+
+from skimage import io
+from skimage.feature import hog
+
+RED = '\033[91m \x1B[3m'
+YELLOW = '\033[93m \x1B[3m'
+GREEN = '\033[92m \x1B[3m'
+RESET = '\033[0m \x1B[0m'
+
+def extract_features(image):
+    return hog(image, orientations=8, pixels_per_cell=(8, 8),
+               cells_per_block=(2, 2), feature_vector=True)
+
+def preprocess_image(image_path):
+    image = io.imread(image_path, as_gray=True)
+    image = image / 255.0
+
+    return image
+
+def preprocess_data(emotions_map, path):
+    features = []
+    labels = []
+    for emotion_name, emotion_label in emotions_map.items():
+        emotion_path = os.path.join(path, emotion_name)
+
+        if not os.path.exists(emotion_path):
+            print(f"{YELLOW}Nie znaleziono folderu {emotion_path}.{RESET}")
+            continue
+
+        for filename in os.listdir(emotion_path):
+            if filename.endswith(".jpg"):
+                image_path = os.path.join(emotion_path, filename)
+
+                features.append(preprocess_image(image_path))
+                labels.append(emotion_label)
+        print(f"{GREEN}Przetworzono klasę {emotion_name}{RESET}")
+
+    return features, labels
